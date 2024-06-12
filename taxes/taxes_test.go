7571ac8954df628da7017979bf4b5e0f7134e3a7
Nathan Youngman <git@nathany.com>
2024-06-11 20:41:45 -0600

    Godoc Example for Income Tax

--- taxes/taxes_test.go
+++ taxes/taxes_test.go
@@ -1,6 +1,9 @@
 package taxes
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func TestIncomeTax(t *testing.T) {
 	result := IncomeTax(44_000)
@@ -8,6 +11,12 @@ func TestIncomeTax(t *testing.T) {
 	if result != 6_455.75 {
 		t.Errorf("Expected $6,455.75, but got %f", result)
 	}
+}
+
+func ExampleIncomeTax() {
+	fmt.Printf("The income tax on $44,000 is $%.2f.", IncomeTax(44_000))
+	// Output:
+	// The income tax on $44,000 is $6455.75.
 }
 
 func TestGrossIncomeForNet(t *testing.T) {
